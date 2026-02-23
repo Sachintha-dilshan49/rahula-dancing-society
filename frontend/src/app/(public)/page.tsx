cdca71253d24d56d77a98640d4e43def62d21536
sachintha dilshan <sachintharcm@gmail.com>
2026-02-23 14:52:20 +0530

    Initial clean monorepo setup with frontend and backend

---
+++ frontend/src/app/(public)/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/features/public/components/hero/Hero";
+import Stats from "@/features/public/components/stats/Stats";
+import About from "@/features/public/components/about/about";
+import Traditions from "@/features/public/components/traditions/Traditions";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Stats />
+      <About />
+      <Traditions />
+    </>
+  );
+}
